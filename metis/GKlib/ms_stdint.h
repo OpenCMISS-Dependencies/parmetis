833961d16faa8237275f39e176f596dcab57575b
sgenie68 <g.soudlenkov@auckland.ac.nz>
2019-11-06 16:05:58 +1300

    Update ms_stdint.h
    Blocked fastXX typedefs

--- metis/GKlib/ms_stdint.h
+++ metis/GKlib/ms_stdint.h
@@ -75,6 +75,7 @@ typedef uint16_t  uint_least16_t;
 typedef uint32_t  uint_least32_t;
 typedef uint64_t  uint_least64_t;
 
+#if 0
 // 7.18.1.3 Fastest minimum-width integer types
 typedef int8_t    int_fast8_t;
 typedef int16_t   int_fast16_t;
@@ -84,6 +85,7 @@ typedef uint8_t   uint_fast8_t;
 typedef uint16_t  uint_fast16_t;
 typedef uint32_t  uint_fast32_t;
 typedef uint64_t  uint_fast64_t;
+#endif
 
 // 7.18.1.4 Integer types capable of holding object pointers
 #ifdef _WIN64 // [
